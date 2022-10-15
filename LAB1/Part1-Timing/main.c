ca7da62f7bf027a06fa4628e2373b910cac7a153
Jiayan Dong <jid001@ucsd.edu>
2022-10-14 17:13:31 -0700

    Exercise 2-4 v0.2

--- LAB1/Part1-Timing/main.c
+++ LAB1/Part1-Timing/main.c
@@ -22,7 +22,7 @@ int main (int ac, char **av) {
     // Allocate a buffer of 64 Bytes
     // the size of an unsigned integer (uint64_t) is 8 Bytes
     // Therefore, we request 8 * 8 Bytes
-    uint64_t *target_buffer = (uint64_t *)malloc(8*sizeof(uint64_t)*1024);
+    uint64_t *target_buffer = (uint64_t *)malloc(8*8*1024*sizeof(uint64_t));
 
     if (NULL == target_buffer) {
         perror("Unable to malloc");
@@ -64,7 +64,7 @@ int main (int ac, char **av) {
         {
             for (int k = 1; k <= 8; k++)
             {
-                tmp = target_buffer[k * 64];
+                tmp = target_buffer[k * 8 * 64];
             }
         }
         
@@ -83,7 +83,7 @@ int main (int ac, char **av) {
         {
             for (int k = 1; k <= 8; k++)
             {
-                tmp = target_buffer[k * 512];
+                tmp = target_buffer[k * 8 * 512];
             }
         }
 
