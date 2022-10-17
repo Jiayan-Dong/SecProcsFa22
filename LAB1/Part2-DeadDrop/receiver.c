2a6d02be1fc74683724e857bd0498ce344ec246a
Jiayan Dong <jid001@ucsd.edu>
2022-10-17 11:09:03 -0700

    receiver.c

--- LAB1/Part2-DeadDrop/receiver.c
+++ LAB1/Part2-DeadDrop/receiver.c
@@ -1,11 +1,23 @@
 
-#include"util.h"
+#include "util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
+
+#define BUFF_SIZE (1 << 21)
 
 int main(int argc, char **argv)
 {
 	// Put your covert channel setup code here
+	void *buf = mmap(NULL, BUFF_SIZE, PROT_READ | PROT_WRITE, MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB, -1, 0);
+
+	if (buf == (void *)-1)
+	{
+		perror("mmap() error\n");
+		exit(EXIT_FAILURE);
+	}
+	*((char *)buf) = 1; // dummy write to trigger page allocation
+
+	volatile unsigned char tmp_chr;
 
 	printf("Please press enter.\n");
 
@@ -15,15 +27,27 @@ int main(int argc, char **argv)
 	printf("Receiver now listening.\n");
 
 	bool listening = true;
-	while (listening) {
-
+	while (listening)
+	{
 		// Put your covert channel code here
+		for (unsigned char i = 0; i < 256; i++)
+		{
+			// Prime
+			for (int j = 0; j < 8; i++)
+			{
+				tmp_chr = ((char *)buf)[(i << 6) + (j << 15)];
+			}
 
+			// Probe
+			for (int j = 0; j < 8; i++)
+			{
+				if(measure_one_block_access_time((uint64_t) &((char *)buf)[(i << 6) + (j << 15)]) > 77)
+					printf("%u\n", i);
+			}
+		}
 	}
 
 	printf("Receiver finished.\n");
 
 	return 0;
 }
-
-
